b1c3d11e67cb9155a5ad125fceb5446ec5b7278d
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-10-11 18:28:44 -0500

    working on codewars exercises

--- codewars.js
+++ codewars.js
@@ -3,18 +3,23 @@
 //     return Math.max(...numbers) + " " + Math.min(...numbers);
 // }
 //
-function getMiddle(s) {
-    let length = s.length;
-    let halfway = length / 2;
-    const oddOrEven = (length) => length % 2 === 0;
-    if (oddOrEven(length) === true) {
-        // middle two
-        return s.substring(halfway - 1, halfway + 1);
-    } else {
-        // middle one
-        return s.substring(halfway, halfway + 1);
-    }
-}
+// function getMiddle(s) {
+//     let length = s.length;
+//     let halfway = length / 2;
+//     const oddOrEven = (length) => length % 2 === 0;
+//     if (oddOrEven(length) === true) {
+//         // middle two
+//         return s.substring(halfway - 1, halfway + 1);
+//     } else {
+//         // middle one
+//         return s.substring(halfway, halfway + 1);
+//     }
+// }
 // console.log(getMiddle("ccdcc"));
 // console.log(getMiddle("aaabbaaa"));
 //
+function findNeedle(haystack) {
+    let position = haystack.indexOf("needle");
+    return "Needle found at position " + position;
+}
+console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]));
