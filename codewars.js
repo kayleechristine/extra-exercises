5641af02f6465e1fd12622e86d862a6a95b4032e
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-10-14 16:34:28 -0500

    working on codewars exercises

--- codewars.js
+++ codewars.js
@@ -11,49 +11,49 @@ function highAndLow(numbers) {
 //
 // * Get the Middle Character *
 //
-// function getMiddle(s) {
-//     let length = s.length;
-//     let halfway = length / 2;
-//     const oddOrEven = (length) => length % 2 === 0;
-//     if (oddOrEven(length) === true) {
-//         // middle two
-//         return s.substring(halfway - 1, halfway + 1);
-//     } else {
-//         // middle one
-//         return s.substring(halfway, halfway + 1);
-//     }
-// }
+function getMiddle(s) {
+    let length = s.length;
+    let halfway = length / 2;
+    const oddOrEven = (length) => length % 2 === 0;
+    if (oddOrEven(length) === true) {
+        // middle two
+        return s.substring(halfway - 1, halfway + 1);
+    } else {
+        // middle one
+        return s.substring(halfway, halfway + 1);
+    }
+}
 // console.log(getMiddle("ccdcc"));
 // console.log(getMiddle("aaabbaaa"));
 //
 // * A Needle in the Haystack *
 //
-// function findNeedle(haystack) {
-//     let position = haystack.indexOf("needle");
-//     return "Needle found at position " + position;
-// }
+function findNeedle(haystack) {
+    let position = haystack.indexOf("needle");
+    return "Needle found at position " + position;
+}
 // console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]));
 //
 // * Abbreviate a Two Word Name *
 //
-// function abbrevName(name){
-//     let space = name.indexOf(" ");
-//     return (name.substring(0, 1) + "." + (name.substring(space+1, space+2))).toUpperCase();
-// }
+function abbrevName(name){
+    let space = name.indexOf(" ");
+    return (name.substring(0, 1) + "." + (name.substring(space+1, space+2))).toUpperCase();
+}
 // console.log(abbrevName("kaylee morris"));
 // console.log(abbrevName("joshua mullins"));
 //
 // * Return Negative *
 //
-// const makeNegative = (num) => num > 0 ? num * -1 : num;
+const makeNegative = (num) => num > 0 ? num * -1 : num;
 //
 // * You Only Need One *
 //
-// const check = (a, x) => a.includes(x);
+const check = (a, x) => a.includes(x);
 //
 // * Find Opposite *
 //
-// const opposite = (number) => -number;
+const opposite = (number) => -number;
 // console.log(opposite(1));
 // console.log(opposite(0));
 // console.log(opposite(-12525220.3325));
@@ -66,9 +66,9 @@ const greet = (name, owner) => name === owner ? "Hello boss" : "Hello guest";
 //
 // * Convert Number to Reversed Array of Digits *
 //
-// function digitize(n) {
-//     n.toString().split("").reverse();
-// }
+function digitize(n) {
+    n.toString().split("").reverse();
+}
 //
 // console.log(digitize(12345));
 
@@ -80,4 +80,53 @@ function spookyTime(starbucks) {
     }
 }
 
-console.log(spookyTime("pumpkin spice latte"));
+// console.log(spookyTime("pumpkin spice latte"));
+
+// * Find Average *
+
+// let array = [];
+
+//  console.log(array.length);
+//
+function findAverage(array) {
+    if (array.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    let average = sum / array.length;
+    return average;
+}
+//
+// console.log(findAverage(array));
+
+// * Count Sheep *
+
+let countSheep = function (num){
+    let sheep = "";
+    for (let i = 1; i <= num; i++) {
+        sheep += i + " sheep...";
+    }
+    return sheep;
+}
+
+// console.log(countSheep(3));
+
+// * Jaden Case *
+
+String.prototype.toJadenCase = function (tweet) {
+    tweet = tweet.split(" ");
+    return tweet;
+    // tweet.forEach(function(word) {
+    //     word.toString();
+    // })
+};
+
+
+
+
+
+
+
