f06bbdb1b43ea30fdb9b9e84cfcd847d99375905
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-10-24 12:30:28 -0500

    working on the doggo project

--- doggos.js
+++ doggos.js
@@ -2,11 +2,11 @@ let btn = document.getElementById('btnClick');
 let image = document.getElementById('dog-pic');
 
 btn.addEventListener('click', function() {
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("https://random.dog/woof.json")
         .then(res => res.json())
         .then(result => {
             console.log(result)
-            image.src = result.message
+            image.src = result.url
     })
         .catch(err => console.log(err));
 })
